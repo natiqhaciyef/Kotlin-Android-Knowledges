dfd093664073f2b754350133847457b32529351d
natiqhaciyef <natiq00h2272@gmail.com>
2023-10-16 22:19:51 +0400

    Leetcode hard problem solved

---
+++ app/src/main/java/com/natiqhaciyef/kotlinandroidknowledges/technical_skills/leetcode/hard/Median of Two Sorted Arrays.kt
@@ -0,0 +1,18 @@
+package com.natiqhaciyef.kotlinandroidknowledges.technical_skills.leetcode.hard
+
+fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
+    val list = nums1.toMutableList()
+    list.addAll(nums2.toMutableList())
+
+    list.sort()
+    val size = list.size
+
+    return if (size % 2 == 0)
+        (list[list.size/2].toDouble() + list[list.size/2 -1].toDouble())/2
+    else
+        list[list.size/2].toDouble()
+}
+
+fun main() {
+
+}
