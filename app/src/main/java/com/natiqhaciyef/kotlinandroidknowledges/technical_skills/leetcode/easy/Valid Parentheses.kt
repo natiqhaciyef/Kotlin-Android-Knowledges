221e9f3605b9c063c1db8a4758b37ae441045502
natiqhaciyef <natiq00h2272@gmail.com>
2023-12-26 23:04:48 +0400

    CustomSharedPref update

---
+++ app/src/main/java/com/natiqhaciyef/kotlinandroidknowledges/technical_skills/leetcode/easy/Valid Parentheses.kt
@@ -0,0 +1,60 @@
+package com.natiqhaciyef.kotlinandroidknowledges.technical_skills.leetcode.easy
+
+fun isValid(sub: String): Boolean {
+    val s = sub.toMutableList().map { it.toString() }.toMutableList()
+    if (s.size <= 1)
+        return false
+
+    while (s.isNotEmpty()) {
+        if (s.contains("(")) {
+            if (s.contains(")") && s.indexOf(")") > s.indexOf("(") && (s.indexOf("(") - s.indexOf(")")) % 2 != 0) {
+                s.removeAt(s.indexOf("("))
+                s.removeAt(s.indexOf(")"))
+                println(s)
+            } else
+                return false
+        }
+
+        if (s.contains("{")) {
+            if (s.contains("}") && s.indexOf("}") > s.indexOf("{") && (s.indexOf("{") - s.indexOf("}")) % 2 != 0) {
+                s.removeAt(s.indexOf("{"))
+                s.removeAt(s.indexOf("}"))
+                println(s)
+            } else
+                return false
+        }
+
+        if (s.contains("[")) {
+            if (s.contains("]") && s.indexOf("]") > s.indexOf("[") && (s.indexOf("[") - s.indexOf("]")) % 2 != 0) {
+                s.removeAt(s.indexOf("["))
+                s.removeAt(s.indexOf("]"))
+
+                println(s)
+            } else
+                return false
+        }
+    }
+    return true
+}
+
+
+fun isValidFast(sub: String): Boolean {
+    val stack = mutableListOf<Char>()
+
+    for (char in sub) {
+        when (char) {
+            '(', '{', '[' -> stack.add(char)
+            ')' -> if (stack.isEmpty() || stack.removeLast() != '(') return false
+            '}' -> if (stack.isEmpty() || stack.removeLast() != '{') return false
+            ']' -> if (stack.isEmpty() || stack.removeLast() != '[') return false
+        }
+    }
+
+    return stack.isEmpty()
+}
+
+fun main() {
+    println(isValid("()[]{}"))
+    println(isValid("([)]{}"))
+    println(isValid("(){}}{"))
+}
