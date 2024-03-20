b5f1fcce2de4697c3de7e3955cd33c11dee0ba1a
natighajiyev <natig.hajiyev1@gmail.com>
2024-03-20 18:40:11 +0400

    ViewModel init type with provider

--- app/src/main/java/com/natiqhaciyef/kotlinandroidknowledges/android/livedata/LiveDataFragment.kt
+++ app/src/main/java/com/natiqhaciyef/kotlinandroidknowledges/android/livedata/LiveDataFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.natiqhaciyef.kotlinandroidknowledges.R
 import com.natiqhaciyef.kotlinandroidknowledges.databinding.FragmentLiveDataBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -25,6 +26,8 @@ class LiveDataFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val viewModel = ViewModelProvider(this)[CustomViewModel::class.java]
+
         binding.apply {
 
         }
