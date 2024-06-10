fdd6c583f55447976489f18d173bba1d34956a02
natighajiyev <natig.hajiyev1@gmail.com>
2024-06-10 21:29:51 +0400

    Database migration created

--- app/src/main/java/com/natiqhaciyef/kotlinandroidknowledges/android/local_db/room/AppDatabase.kt
+++ app/src/main/java/com/natiqhaciyef/kotlinandroidknowledges/android/local_db/room/AppDatabase.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
 
 @Database(entities = [CountryEntity::class], version = 1)
@@ -17,13 +19,23 @@ abstract class AppDatabase : RoomDatabase() {
 
         private const val lock = "Locked"
         operator fun invoke(context: Context) = databaseInstance ?: synchronized(lock) {
-            databaseInstance ?: createInstance(context).also {  databaseInstance = it }
+            databaseInstance ?: createInstance(context).also { databaseInstance = it }
         }
 
         private fun createInstance(context: Context) = Room.databaseBuilder(
             context.applicationContext,
             AppDatabase::class.java,
             "app_database"
-        ).build()
+        )
+            .addMigrations(MIGRATION_1_2)
+            .build()
+
+        // manual migration
+        private const val DATABASE_NAME = "app_database.db"
+        val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE notes_database ADD COLUMN creationDate INTEGER NOT NULL DEFAULT 0")
+            }
+        }
     }
 }
