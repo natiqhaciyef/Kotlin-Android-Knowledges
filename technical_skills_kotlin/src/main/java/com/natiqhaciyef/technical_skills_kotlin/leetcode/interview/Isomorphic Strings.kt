153062e5d8b4439a28ce1cc20d3d4cd6f8f5c848
natiqhaciyef <natiq00h2272@gmail.com>
2024-12-06 23:40:53 +0100

    Algorithm update

---
+++ technical_skills_kotlin/src/main/java/com/natiqhaciyef/technical_skills_kotlin/leetcode/interview/Isomorphic Strings.kt
@@ -0,0 +1,37 @@
+package com.natiqhaciyef.technical_skills_kotlin.leetcode.interview
+
+fun isIsomorphic(s: String, t: String): Boolean {
+    val hashMap = hashMapOf<Char, Char>()
+
+    if (s.length != t.length)
+        return false
+
+    for (index in s.indices) {
+//        println("S: ${s[index]}")
+//        println("T: ${t[index]}")
+
+        if (hashMap.containsKey(s[index])) {
+//            println("Hash: ${hashMap[s[index]]}")
+
+            if (hashMap[s[index]] != t[index]) {
+//                println("In")
+                return false
+            }
+        } else {
+            if (!hashMap.values.contains(t[index]))
+                hashMap[s[index]] = t[index]
+            else
+                return false
+        }
+
+    }
+
+    return true
+}
+
+fun main() {
+    val s1 = "babc"
+    val t1 = "baba"
+
+    println(isIsomorphic(s1, t1))
+}
---
+++ technical_skills_kotlin/src/main/java/com/natiqhaciyef/technical_skills_kotlin/leetcode/interview/Ransom Note.kt
@@ -0,0 +1,20 @@
+package com.natiqhaciyef.technical_skills_kotlin.leetcode.interview
+
+
+fun canConstruct(ransomNote: String, magazine: String): Boolean {
+    val hashMap = hashMapOf<Char, Int>()
+
+    for (element in ransomNote) {
+        if (hashMap.containsKey(element))
+            hashMap[element] = hashMap[element]!! + 1
+        else
+            hashMap[element] = 1
+    }
+
+    for (letter in magazine) {
+        if (hashMap.containsKey(letter))
+            hashMap[letter] = hashMap[letter]!! - 1
+    }
+
+    return hashMap.values.all { it <= 0 }
+}
