2030270cd8810843ba9756a39100cb600366ea99
natiqhaciyef <natiq00h2272@gmail.com>
2024-12-10 23:41:00 +0100

    Algorithm update

---
+++ technical_skills_kotlin/src/main/java/com/natiqhaciyef/technical_skills_kotlin/leetcode/interview/Happy Number.kt
@@ -0,0 +1,38 @@
+package com.natiqhaciyef.technical_skills_kotlin.leetcode.interview
+
+
+fun isHappy(n: Int): Boolean {
+
+    return try {
+        var result = calculateNumberSquare(n)
+        var count = 0
+        while (result != 1 && count < 100) {
+            count += 1
+            result = calculateNumberSquare(result)
+//            println("Result: $result")
+        }
+
+        return count < 100
+    } catch (e: Exception) {
+        false
+    }
+}
+
+
+fun calculateNumberSquare(n: Int): Int {
+    var x = n
+    var result = 0
+    while (x > 0) {
+        val c = (x % 10)
+        result += (c * c)
+        x -= c
+        x /= 10
+    }
+
+    return result
+}
+
+fun main() {
+    val num = 19
+    isHappy(num)
+}
---
+++ technical_skills_kotlin/src/main/java/com/natiqhaciyef/technical_skills_kotlin/leetcode/interview/Two Sum.kt
@@ -0,0 +1,23 @@
+package com.natiqhaciyef.technical_skills_kotlin.leetcode.interview
+
+fun twoSum(nums: IntArray, target: Int): IntArray {
+
+    for (i in 0 until nums.size - 1) {
+        for (j in i + 1 until nums.size) {
+            if (nums[i] + nums[j] == target) {
+                println("Num 1: ${nums[i]}")
+                println("Num 2: ${nums[j]}")
+
+                return intArrayOf(i, j)
+            }
+        }
+    }
+
+    return intArrayOf(0, 0)
+}
+
+
+fun main() {
+    val nums1 = intArrayOf(3, 2, 4)
+    println(twoSum(nums1, 6))
+}
