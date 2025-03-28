2b1b78d05571f84dde3403f89fcdecdd57adc1cb
natiqhaciyef <natiq00h2272@gmail.com>
2025-03-28 21:22:57 +0100

    Leetcode

---
+++ technical_skills_kotlin/src/main/java/com/natiqhaciyef/technical_skills_kotlin/leetcode/interview/Reverse Words in String.kt
@@ -0,0 +1,27 @@
+package com.natiqhaciyef.technical_skills_kotlin.leetcode.interview
+
+fun reverseWords(s: String): String {
+    var left = 0
+    var right = s.length -1
+    val current = StringBuilder(s)
+
+    current.append("alma")
+
+    while(left < right){
+        val temp = current[left]
+        current[left] = current[right]
+        current[right] = temp
+
+        left +=1
+        right +=1
+    }
+
+    return s
+}
+
+fun main() {
+    val str = StringBuilder("alma zad")
+    str.insert(0, "geri ")
+    str.removeRange(str.length-1 until str.length)
+    println(str.toString())
+}
