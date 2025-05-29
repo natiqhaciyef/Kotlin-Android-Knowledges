e0383d2e22b0340a1f45fb243af1f2ad62cb1db5
natiqhaciyef <natiq00h2272@gmail.com>
2025-05-29 20:17:09 +0200

    Leetcode

---
+++ technical_skills_kotlin/src/main/java/com/natiqhaciyef/technical_skills_kotlin/leetcode/medium/Gray Code.kt
@@ -0,0 +1,18 @@
+package com.natiqhaciyef.technical_skills_kotlin.leetcode.medium
+
+
+fun grayCode(n: Int): List<Int> {
+    val result = mutableListOf<Int>()
+    val size = 1 shl n
+
+    for (i in 0 until size) {
+        result.add(i xor (i shr 1))
+    }
+
+    return result
+}
+
+
+fun main() {
+    grayCode(4)
+}
